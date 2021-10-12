2e12e14065f6e33c7e378bf14a73c908834a8177
Jess Mark Sevilleja <jess@clinica.dev>
2021-10-12 12:46:58 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Amplify from '@aws-amplify/core'
+import config from './src/aws-exports'
+Amplify.configure(config)
+
+import React, {useState, useEffect} from 'react'
+import {Text, View, TextInput, Button} from 'react-native'
+import {DataStore} from '@aws-amplify/datastore'
+import {Message} from './src/models'
+
+const initialState = {color: 'black', title: ''}
 
 export default function App() {
+  const [formState, updateFormState] = useState(initialState)
+  const [messages, updateMessages] = useState([])
+
+  useEffect(() => {
+    fetchMessages()
+    const subscription = DataStore.observe(Message).subscribe(() => fetchMessages())
+    return () => subscription.unsubscribe()
+  })
+
+  function onChangeText(key, value) {
+    updateFormState({...formState, [key]: value})
+  }
+
+  async function fetchMessages() {
+    const messages = await DataStore.query(Message)
+    updateMessages(messages)
+  }
+  async function createMessage() {
+    if (!formState.title) return
+    await DataStore.save(new Message({...formState}))
+    updateFormState(initialState)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={container}>
+      <Text style={heading}>Real Time Message Board</Text>
+      <TextInput
+        onChangeText={v => onChangeText('title', v)}
+        placeholder='Message title'
+        value={formState.title}
+        style={input}
+      />
+      <TextInput
+        onChangeText={v => onChangeText('color', v)}
+        placeholder='Message color'
+        value={formState.color}
+        style={input}
+        autoCapitalize='none'
+      />
+      <Text>Color: <Text style={{fontWeight: 'bold', color: formState.color}}>{formState.color}</Text></Text>
+      <Button onPress={createMessage} title='Create Message' />
+      {
+        messages.map(message => (
+          <View key={message.id} style={{...messageStyle, backgroundColor: message.color}}>
+            <View style={messageBg}>
+              <Text style={messageTitle}>{message.title}</Text>
+            </View>
+          </View>
+        ))
+      }
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const container = {padding: 20, paddingTop: 80}
+const input = {marginBottom: 10, padding: 7, backgroundColor: '#ddd'}
+const heading = {fontWeight: 'normal', fontSize: 40}
+const messageBg = {backgroundColor: 'white'}
+const messageStyle = {padding: 20, marginTop: 7, borderRadius: 4}
+const messageTitle = {margin: 0, padding: 9, fontSize: 20}
---
+++ src/models/index.d.ts
@@ -0,0 +1,19 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+type MessageMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class Message {
+  readonly id: string;
+  readonly title: string;
+  readonly color?: string;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Message, MessageMetaData>);
+  static copyOf(source: Message, mutator: (draft: MutableModel<Message, MessageMetaData>) => MutableModel<Message, MessageMetaData> | void): Message;
+}
